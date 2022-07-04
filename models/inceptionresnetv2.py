81517d1bbb1633df5a099d078a6b08c92b7c0cf9
tungna292 <tungna292@gmail.com>
2022-07-04 15:39:56 +0700

    update

---
+++ models/inceptionresnetv2.py
@@ -0,0 +1,56 @@
+from __future__ import print_function, division, absolute_import
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+import os
+import sys
+from pretrainedmodels.models.inceptionresnetv2 import InceptionResNetV2
+
+# __all__ = ['InceptionResNetV2', 'inceptionresnetv2']
+
+pretrained_settings = {
+    'inceptionresnetv2': {
+        'imagenet': {
+            'url': 'http://data.lip6.fr/cadene/pretrainedmodels/inceptionresnetv2-520b38e4.pth',
+            'input_space': 'RGB',
+            'input_size': [3, 299, 299],
+            'input_range': [0, 1],
+            'mean': [0.5, 0.5, 0.5],
+            'std': [0.5, 0.5, 0.5],
+            'num_classes': 1000
+        }
+    }
+}
+
+
+def inceptionresnetv2(num_classes=1000, pretrained='imagenet'):
+    r"""InceptionResNetV2 model architecture from the
+    `"InceptionV4, Inception-ResNet..." <https://arxiv.org/abs/1602.07261>`_ paper.
+    """
+    if pretrained:
+        settings = pretrained_settings['inceptionresnetv2'][pretrained]
+        assert num_classes == settings['num_classes'], \
+            "num_classes should be {}, but is {}".format(settings['num_classes'], num_classes)
+
+        # both 'imagenet'&'imagenet+background' are loaded from same parameters
+        model = InceptionResNetV2(num_classes=1001)
+        # model.load_state_dict(model_zoo.load_url(settings['url']))
+        model.load_state_dict(torch.load('checkpoints/inceptionresnetv2-520b38e4.pth'))
+
+        if pretrained == 'imagenet':
+            new_last_linear = nn.Linear(1536, 1000)
+            new_last_linear.weight.data = model.last_linear.weight.data[1:]
+            new_last_linear.bias.data = model.last_linear.bias.data[1:]
+            model.last_linear = new_last_linear
+
+        model.input_space = settings['input_space']
+        model.input_size = settings['input_size']
+        model.input_range = settings['input_range']
+
+        model.mean = settings['mean']
+        model.std = settings['std']
+    else:
+        model = InceptionResNetV2(num_classes=num_classes)
+    return model
+
